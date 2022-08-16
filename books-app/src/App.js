2506174091dcf3d9e06bc3a7fc9ac477006745a2
MonkeySki <rebeccafredericknz@gmail.com>
2022-08-16 18:39:08 +1200

    eslint, hustky and more

--- books-app/src/App.js
+++ books-app/src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-
-import CreateBook from './components/CreateBook';
-import ShowBookList from './components/ShowBookList';
-import ShowBookDetails from './components/ShowBookDetails';
-import UpdateBookInfo from './components/UpdateBookInfo';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import CreateBook from './components/CreateBook'
+import ShowBookList from './components/ShowBookList'
+import ShowBookDetails from './components/ShowBookDetails'
+import UpdateBookInfo from './components/UpdateBookInfo'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Routes>
-          <Route path='/' element={<ShowBookList></ShowBookList>} />
-          <Route path='/create-book' element={<CreateBook></CreateBook>} />
-          <Route path='/edit-book/:id' element={<UpdateBookInfo></UpdateBookInfo>} />
-          <Route path='/show-book/:id' element={<ShowBookDetails></ShowBookDetails>} />
+          <Routes
+            <Route path='/' element={<ShowBookList></ShowBookList>} />
+            <Route path='/create-book' element={<CreateBook></CreateBook>} />
+            <Route path='/edit-book/:id' element={<UpdateBookInfo></UpdateBookInfo>} />
+            <Route path='/show-book/:id' element={<ShowBookDetails></ShowBookDetails>} />
           </Routes>
         </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default App;
+export default App
