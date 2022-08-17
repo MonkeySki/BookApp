9693555b7e10597e8b2cfda7bea983368a49eb79
MonkeySki <rebeccafredericknz@gmail.com>
2022-08-18 09:59:32 +1200

    Seeing if database links to heroku

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 // routes
-const books = require('./routes/api/books');
+const books = require("./routes/api/books");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8082;
 
 // cors config - allow same origin
 const corsOptions = {
@@ -21,20 +21,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // use routes here
-//app.use('/api/repo', repo);
-app.use('/api/books', books);
+// app.use('/api/repo', repo);
+app.use("/api/books", books);
 
 // Connect Database
 connectDB();
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // serve front-end client from build folder
-  app.use(express.static(__dirname+'/books-app/build'));
-  app.get('*', (req, res) =>{
-    res.sendFile(__dirname+'/books-app/build/index.html')
+  app.use(express.static(`${__dirname}/books-app/build`));
+  app.get("*", (req, res) => {
+    res.sendFile(`${__dirname}/books-app/build/index.html`);
   });
-  
 } else {
-  app.get('*', (req, res) => res.send(`API running on port ${port}`));
+  app.get("*", (req, res) => res.send(`API running on port ${port}`));
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
